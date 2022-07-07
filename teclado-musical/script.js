6db43977ee8e1dbdd24eb7c7b0eab771c9011fd3
Caligula <caio.ctv@gmail.com>
2022-07-07 19:39:21 -0300

    importação inicial

--- teclado-musical/script.js
+++ teclado-musical/script.js
@@ -1,14 +1,17 @@
 const teclas = document.querySelectorAll(".oitava > div")
 
-function desmarcar(tecla) {
-        tecla.classList.remove("selecionada")
-    }
-
-teclas.forEach(function(tecla){
-    tecla.onmousedown = function() {
-        tecla.classList.add("selecionada")
-    }
-
-    tecla.onmouseup = () => desmarcar(tecla)
-    tecla.onmouseleave = ()=> desmarcar(tecla)
+teclas.forEach(tecla => {
+    tecla.addEventListener('click', () => playNote(tecla))
 })
+
+function playNote(tecla) {
+    const noteAudio = document.getElementById(tecla.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    tecla.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+        tecla.classList.remove('active')
+    })
+}
+
+
