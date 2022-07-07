7f283256c97659a84f4c45806cb2a1b3f1179075
Caligula <caio.ctv@gmail.com>
2022-07-06 21:02:23 -0300

    importacao inicial

--- teclado-musical/script.js
+++ teclado-musical/script.js
@@ -1,83 +1,14 @@
 const teclas = document.querySelectorAll(".oitava > div")
-const botoes = document.querySelectorAll(".botoes > div")
 
-const acordes = [ 
-    document.querySelectorAll("#c, #e, #g"), 
-    document.querySelectorAll("#d, #f, #a"), 
-    document.querySelectorAll("#e, #g, #b")
-]
+function desmarcar(tecla) {
+        tecla.classList.remove("selecionada")
+    }
 
-botoes.forEach(function(botao){
-    botao.onmousedown = function() {
-        botao.classList.add("selecionada")
+teclas.forEach(function(tecla){
+    tecla.onmousedown = function() {
+        tecla.classList.add("selecionada")
     }
-    botao.onmouseup = function() {
-        botao.classList.remove("selecionada")
-    }
+
+    tecla.onmouseup = () => desmarcar(tecla)
+    tecla.onmouseleave = ()=> desmarcar(tecla)
 })
-
-
-
-
-
-//function down(chord) {
-//    chord.classList.remove("selecionada")
-//}
-//function up(chord) {
-//    chord.classList.add("selecionada")
-//}
-
-
-//botoes.forEach(function(botao) {
-//    botao.onmousedown = function() {
-//        botao.classList.add("selecionada")
-//    }
-//
-//    botao.onmouseup = function() {
-//        console.log("onmouseup")
-//    }
-//})
-
-//function funcoes(acordes) {
-//    chord(acordes)
-//    remover(acordes)
-//}
-//
-//function remover(acordes) {
-//    acordes.classList.remove("selecionada")
-//    console.log("desmarcar")
-//}
-//function chord(acordes) {
-//    acordes.classList.add("selecionada")
-//    console.log("marcar")
-//}
-
-//function chord(acordes) {
-//    for (let index = 0; index < acordes.length; index++) {
-//        acordes[index].classList.add("selecionada")
-//    }
-//}
-//function remover(acordes) {
-//    for (let index = 0; index < acordes.length; index++) {
-//        acordes[index].classList.remove("selecionada")   
-//    }
-//} document.querySelectorAll("#c, #e, #g")
-
-
-//const acorde = document.querySelector("#chord")
-//
-//const acordes = [ 
-//    document.querySelectorAll("#c, #e, #g"), 
-//    document.querySelectorAll("#d, #f, #a"), 
-//    document.querySelectorAll("#e, #g, #b")
-//]
-//
-//
-//function chord(acordes) {
-//    
-//    for (let index = 0; index < acordes.length; index++) {
-//        acordes[index].classList.add("selecionada")
-//        
-//    }
-//}
-
